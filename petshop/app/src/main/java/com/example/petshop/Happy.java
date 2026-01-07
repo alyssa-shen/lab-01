99343216c556ab57b21aa24d51a578d09065b445
alyssaS <alyssashens@gmail.com>
2026-01-06 18:25:29 -0700

    lab demo complete

---
+++ petshop/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,11 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood{
+
+    public Happy(Date moodDate) {
+        super(moodDate);
+    }
+
+}
---
+++ petshop/app/src/main/java/com/example/petshop/Sad.java
@@ -0,0 +1,9 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Sad extends Mood {
+    public Sad(Date moodDate) {
+        super(moodDate);
+    }
+}
